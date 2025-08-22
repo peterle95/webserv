5378fb76dc2b1ad1b68daa33b8db92dd68dfbf6d
Peter Moelzer <moelzerpeter@gmail.com>
2025-08-22 18:00:28 +0200

    expanding structure for HTTP parser

--- include/HTTPparser.hpp
+++ include/HTTPparser.hpp
@@ -16,13 +16,16 @@
 class HTTPparser
 {
     private:
-        std::string _HTTPrequest;
+        std::string _HTTPrequest; // raw requested data, this might need to change type because large requests like POST might be too large for a string
         std::vector<std::string> _lines;
         std::string _method; // HTTP Method GET, POST, DELETE
         std::string _path; // Path of the requested resource
         std::string _version; // HTTP Version
         std::map<std::string, std::string> _headers; // Headers
         std::string _body; // Body
+        std::string _buffer; // buffer to account for the sequential and potentially incremental nature of HTTP requests for non-blocking servers
+        size_t _contentLength; // content length of the request
+        std::string _errorStatusCode;         
     public:
         HTTPparser();
         ~HTTPparser();
