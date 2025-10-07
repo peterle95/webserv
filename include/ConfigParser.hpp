f785052104646b4ae3880acd4cdc8d781ad503f7
shrutigangal <53692037+shrutigangal@users.noreply.github.com>
2025-10-07 11:16:30 +0200

    Update ConfigParser.hpp

--- include/ConfigParser.hpp
+++ include/ConfigParser.hpp
@@ -85,10 +85,7 @@ class ConfigParser
         //ServerName addition -Shruti
         const std::string&  getServerName() const;
     	const std::map<std::string, LocationConfig> &getLocations() const;
-<<<<<<< HEAD
-=======
         const std::string& getErrorPage(int status_code) const;
->>>>>>> c0886c7 (Resouce.cpp)
         // TODO: implement error handling
         // TODO: implement parsing more directives (directives are the lines in the config file)
     	// Location handling
@@ -111,3 +108,4 @@ std::string strip_comment(const std::string &s);
 
 
 #endif
+
